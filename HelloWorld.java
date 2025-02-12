f1b4e894095ddd3afc942a5cef1ecb17e2de298c
milo <42156458+fox-lupus@users.noreply.github.com>
2025-02-12 16:51:49 -0500

    removed some old code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("hi, Alice"); 
-        System.out.println("hi, Bob"); 
-        System.out.println("hi, Alice"); 
-        System.out.println("hi, Bob"); 
-        
-        System.out.println("hi, Charlie"); 
-        System.out.println("hi, Denise"); 
-        System.out.println("hi, Ester"); 
     }
 }
