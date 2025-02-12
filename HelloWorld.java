5d0e36b53b81df7689aece8ee442a7fb35913d13
milo <42156458+fox-lupus@users.noreply.github.com>
2025-02-12 16:50:03 -0500

    init commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        System.out.println("hi, Alice"); 
+        System.out.println("hi, Bob"); 
+        System.out.println("hi, Alice"); 
+        System.out.println("hi, Bob"); 
+        
+        System.out.println("hi, Charlie"); 
+        System.out.println("hi, Denise"); 
+        System.out.println("hi, Ester"); 
+    }
+}
