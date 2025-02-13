2a5fc840932bee3f169a71e102e52e923d453ac5
milo <42156458+fox-lupus@users.noreply.github.com>
2025-02-12 19:51:29 -0500

    Closes printdate
    now prints date along with helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        
+        System.out.println(new Date().toString());
     }
 }
