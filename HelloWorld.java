c9b16eac9a16c8ad7769a25a2744c8bc1a1da9aa
milo <42156458+fox-lupus@users.noreply.github.com>
2025-02-12 19:56:37 -0500

    closes printfreememory
    prints the current free memory of the JVM to the console

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+
+        
+        Runtime runtime = Runtime.getRuntime();
+        System.out.printf("free memory for JVM %d (MB) \n", runtime.freeMemory() / (1024 * 1024) );
     }
 }
